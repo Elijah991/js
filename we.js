e81da550ab58372c669753bf1d3e116ef39c7a7f
Elijah991 <evansampaire@.com>
2024-05-17 23:23:21 +0300

    first commit

---
+++ we.js
@@ -0,0 +1,8 @@
+function calculateNumber(num) {
+    return num * 2 + 5;
+  }
+  
+  // Example usage:
+  const inputNumber = 10;
+  const result = calculateNumber(inputNumber);
+  console.log(`The result is: ${result}`);
